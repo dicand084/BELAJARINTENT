7d566b25230b7d979e4d86e08fb50c3d54cb4763
unknown <dicand82@gmail.com>
2023-03-29 23:12:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarintentdiki/PindahDataActivity.kt
@@ -0,0 +1,25 @@
+package com.example.belajarintentdiki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class PindahDataActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_TEXT = "extra_text"
+    }
+
+    private lateinit var tvpenerima : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pindah_data)
+
+        tvpenerima = findViewById(R.id.tv_terima_data)
+
+        val tvReceived = intent.getStringExtra(EXTRA_TEXT)
+        val text = "Text yang dikirim :$tvReceived"
+
+        tvpenerima.text = text
+    }
+}
